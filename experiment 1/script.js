19d428c82d02622592b47b3325cd64b84f6d1904
abhitanshu06 <abhitanshu.25b01010681@abes.ac.in>
2026-04-16 11:08:18 +0530

    exp1

---
+++ experiment 1/script.js
@@ -0,0 +1,35 @@
+function calculateResult(){
+    let n = document.getElementById("subjects").value;
+    let total=0;
+    for(let i=1; i<=n; i++){
+        let marks = parseFloat(prompt("Enter marks of subjects " + i));   // Pop up shows //
+        total = total + marks;                            // ParseFloat is used for type conversion//
+    }
+    let average = total / n;
+    let grade;
+    let result;
+    if(average >= 90){
+        grade = "A+";
+    }
+    else if(average >= 75){
+        grade = "A";
+    }
+    else if(average >= 60){
+        grade = "B";
+    }
+    else if(average >= 50){
+        grade = "C";
+    }
+    else {
+        grade = "F";
+    }
+    if(average >= 40){
+        result = "PASS";
+    }
+    else {
+        result = "FAIL";
+    }
+    document.getElementById("result").innerHTML = "Total marks: " + total + "<br>" +
+    "Average marks: " + average.toFixed(2) + "<br>" + "Grade: " + grade + "<br>" +
+    "Result: " + result;
+}
